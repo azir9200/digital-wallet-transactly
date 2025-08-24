480482d2d36c56d545ff9f448430f247fc98828c
Azir@9200 <aziruddin83@hotmail.com>
2025-08-24 08:41:48 +0100

    created hero section for home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import FeaturesSection from "@/components/sections/FeaturesSection";
+import HeroSection from "@/components/sections/HeroSection";
+
+const Home = () => {
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
+};
+
+export default Home;
